2a6f675a3f58f7cd84961116906bf049b234dad0
kasra <kasrakhosravi2@gmail.com>
2020-10-19 16:56:22 +0200

    fix url in config.php

--- app/functions/config.php
+++ app/functions/config.php
@@ -3,5 +3,5 @@ $host = "f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "kktcus5uvdthost7";
 $password = "f6qafu5t1mhh2kzo";
 $dbname = "yrwshvme1tvosi4r";
-$url = "https://reply.so/app";
+$url = "https://agile-stream-41612.herokuapp.com/app";
 ?>
